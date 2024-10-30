166afc4884e0becbd4d43721dab298aa28e197ca
roman9234 <roman_lesovoy@mail.ru>
2024-10-30 17:14:13 +0300

    completed lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -2,6 +2,7 @@ from random import randint, random
 
 from being import Being
 from methods import breed, print_beings
+import matplotlib.pyplot as plt
 
 children_amont = 2
 genes_amount = 10
@@ -12,21 +13,36 @@ mutation_probability = 0.2
 
 beings = []
 
-for i in range(population):
-    beings.append(Being([randint(0, 1) for x in range(genes_amount)]))
+max_mutations = 7
 
-i = 0
-while beings[0].get_guality() != genes_amount:
-    beings = sorted(beings, key=lambda x: x.get_guality(), reverse=True)
-    for q in range(0, population, 2):
-        being1 = beings[q]
-        being2 = beings[q + 1]
+iterations_list = []
+mutations_list = [x / 10 for x in range(max_mutations)]
 
-        child1, child2 = breed(being1, being2, random_mutation_probability=0.01)
-        i += 1
-        beings[q] = child1
-        beings[q + 1] = child2
-    print_beings(beings)
+for mutations in range(0,max_mutations):
 
-print_beings(beings)
-print(f"на выведение идеального Being ушло {i} итераций")
+    beings = []
+    for i in range(population):
+        beings.append(Being([randint(0, 1) for x in range(genes_amount)]))
+
+    i = 0
+    while beings[0].get_guality() != genes_amount:
+        beings = sorted(beings, key=lambda x: x.get_guality(), reverse=True)
+        for q in range(0, population, 2):
+            being1 = beings[q]
+            being2 = beings[q + 1]
+
+            # child1, child2 = breed(being1, being2, random_mutation_probability=0.01)
+            child1, child2 = breed(being1, being2, random_mutation_probability=mutations/10)
+            i += 1
+            beings[q] = child1
+            beings[q + 1] = child2
+        # print_beings(beings)
+    iterations_list.append(i)
+    # print_beings(beings)
+
+print(mutations_list)
+print(iterations_list)
+plt.xticks(mutations_list)
+plt.plot(mutations_list, iterations_list)
+plt.show()
+# print(f"на выведение идеального Being ушло {i} итераций")
